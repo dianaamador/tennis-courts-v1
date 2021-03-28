75cba14ed90da9a7881929b32687392f981a5366
Diana <diana.a.barreto@gmail.com>
2021-03-28 14:02:51 -0300

    Complete test commit.

--- src/main/java/com/tenniscourts/schedules/ScheduleController.java
+++ src/main/java/com/tenniscourts/schedules/ScheduleController.java
@@ -1,8 +1,11 @@
 package com.tenniscourts.schedules;
 
 import com.tenniscourts.config.BaseRestController;
+import io.swagger.annotations.SwaggerDefinition;
 import lombok.AllArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -10,18 +13,20 @@ import java.time.LocalTime;
 import java.util.List;
 
 @AllArgsConstructor
+@RestController
+@RequestMapping("/schedule")
 public class ScheduleController extends BaseRestController {
 
     private final ScheduleService scheduleService;
 
-    //TODO: implement rest and swagger
-    public ResponseEntity<Void> addScheduleTennisCourt(CreateScheduleRequestDTO createScheduleRequestDTO) {
+    @PostMapping(value = "/add")
+    public ResponseEntity<Void> addScheduleTennisCourt(@RequestBody CreateScheduleRequestDTO createScheduleRequestDTO) {
         return ResponseEntity.created(locationByEntity(scheduleService.addSchedule(createScheduleRequestDTO.getTennisCourtId(), createScheduleRequestDTO).getId())).build();
     }
 
-    //TODO: implement rest and swagger
-    public ResponseEntity<List<ScheduleDTO>> findSchedulesByDates(LocalDate startDate,
-                                                                  LocalDate endDate) {
+    @GetMapping(value = "/findByDates/{startDate}/{endDate}")
+    public ResponseEntity<List<ScheduleDTO>> findSchedulesByDates(@PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
+                                                                  @PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate) {
         return ResponseEntity.ok(scheduleService.findSchedulesByDates(LocalDateTime.of(startDate, LocalTime.of(0, 0)), LocalDateTime.of(endDate, LocalTime.of(23, 59))));
     }
 
--- src/main/java/com/tenniscourts/schedules/ScheduleMapper.java
+++ src/main/java/com/tenniscourts/schedules/ScheduleMapper.java
@@ -1,6 +1,7 @@
 package com.tenniscourts.schedules;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
@@ -10,6 +11,9 @@ public interface ScheduleMapper {
     Schedule map(ScheduleDTO source);
 
     ScheduleDTO map(Schedule source);
+
+    @Mapping(target = "tennisCourt.id", source = "tennisCourtId")
+    Schedule map(CreateScheduleRequestDTO source);
 
     List<ScheduleDTO> map(List<Schedule> source);
 }
